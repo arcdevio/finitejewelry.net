8b3f04f63eda907cd89d290617eb9ba563afd19d
AlexanderElias <alex.steven.elias@gmail.com>
2016-01-25 13:42:58 -0700

    optimization, keywords

--- static/js/master.js
+++ static/js/master.js
@@ -1,9 +1,8 @@
 (function(){
 'use strict';
-	// 'ngSanitize'
-	angular.module('master',['ngMaterial','angularUtils.directives.dirPagination']).controller('MasterController',['$sce','$http','$location','$timeout',MasterController]);
+	angular.module('master',['ngMaterial','angularUtils.directives.dirPagination']).controller('MasterController',['$http','$location',MasterController]);
 
-	function MasterController ($sce,$http,$location,$timeout) {
+	function MasterController ($http,$location) {
 		var self = this;
 		var pathname = window.location.pathname;
 
@@ -20,14 +19,12 @@
 			case '/contact': self.page = 'contact';
 				break;
 			default: self.page = 'jewelry';
-
 		}
-
 
 		/*
 			item.marko
 		*/
-		if (pathname != '/jewelry' && pathname != '/contact') {
+		if (pathname != '/' && pathname != '/jewelry' && pathname != '/contact' && pathname != '/about') {
 			var activeImage;
 			var firstImage;
 			var lastImage;
@@ -74,7 +71,6 @@
 			};
 		}
 
-
 		/*
 			jewelry.marko
 		*/
@@ -91,7 +87,6 @@
 			self.sort = sort;
 			self.items = null;
 			self.productsEmptyOrError = true;
-			self.trustAsHtml = trustAsHtml;
 
 			$http.get('/api/getAllProducts').then(
 				function (success) {
@@ -107,12 +102,7 @@
 					console.log(error);
 				}
 			);
-
-			var trustAsHtml = function (html) {
-				return $sce.trustAsHtml(html);
-			};
 		}
-
 
 		/*
 			contact.marko
@@ -140,78 +130,5 @@
 				}
 			};
 		}
-
-
 	}
 })();
-
-// var items = [
-	// {
-	// 	"_id": ObjectID(),
-	// 	"title": 'Silver Chain With Onyx',
-	// 	"description": 'This chain has a classic style with with a bold undertone',
-	// 	"price": 10,
-	// 	"buyLink": 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BHUGERAYVVD5J',
-	// 	"images": [
-	// 		{ "src": '/products/1.jpg', "alt": '1', "index": 0}, { "src": '/products/2.jpg', "alt": '2', "index": 1},{ "src": '/products/3.jpg', "alt": '3', "index": 2}
-	// 	]
-	// },
-	// {
-	// 	"_id": ObjectID(),
-	// 	"title": 'Gold Chain With Onyx',
-	// 	"description": 'This chain has a classic style with with a bold undertone',
-	// 	"price": 15,
-	// 	"buyLink": 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BHUGERAYVVD5J',
-	// 	"images": [
-	// 		{ "src": '/products/1.jpg', "alt": '1', "index": 0}, { "src": '/products/2.jpg', "alt": '2', "index": 1},{ "src": '/products/3.jpg', "alt": '3', "index": 2}
-	// 	]
-	// },
-	// {
-	// 	"_id": ObjectID(),
-	// 	"title": 'Silver Chain Black Stones',
-	// 	"description": 'This chain has a classic style with with a bold undertone',
-	// 	"price": 20,
-	// 	"buyLink": 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BHUGERAYVVD5J',
-	// 	"images": [
-	// 		{ "src": '/products/1.jpg', "alt": '1', "index": 0}, { "src": '/products/2.jpg', "alt": '2', "index": 1},{ "src": '/products/3.jpg', "alt": '3', "index": 2}
-	// 	]
-	// },
-	// {
-	// 	title: 'Gold Chain With Onyx',
-	// 	description: 'This chain has a classic style with with a edgy feel',
-	// 	price: 20
-	// },
-	// {
-	// 	title: 'Silver Chain With Onyx',
-	// 	description: 'This chain has a classic style with with a bold undertone',
-	// 	price: 10
-	// },
-	// {
-	// 	title: 'Gold Chain With Onyx',
-	// 	description: 'This chain has a classic style with with a edgy feel',
-	// 	price: 20
-	// },
-	// {
-	// 	title: 'Silver Chain With Onyx',
-	// 	description: 'This chain has a classic style with with a bold undertone',
-	// 	price: 10
-	// },
-	// {
-	// 	title: 'Gold Chain With Onyx',
-	// 	description: 'This chain has a classic style with with a edgy feel',
-	// 	price: 20
-	// },
-	// {
-	// 	title: 'Silver Chain With Onyx',
-	// 	description: 'This chain has a classic style with with a bold undertone',
-	// 	price: 10
-	// },
-	// {
-	// 	title: 'Gold Chain With Onyx',
-	// 	description: 'This chain has a classic style with with a edgy feel',
-	// 	price: 20
-	// }
-// ];
-
-
-//https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BHUGERAYVVD5J
