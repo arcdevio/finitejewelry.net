475a7cdc92c411b3f37a3361abd7f13ead4167be
AlexanderElias <alex.steven.elias@gmail.com>
2016-01-25 12:54:58 -0700

    changed bg image

--- includes/index.marko.js
+++ includes/index.marko.js
@@ -4,7 +4,7 @@ function create(__helpers) {
       notEmpty = __helpers.ne;
 
   return function render(data, out) {
-    out.w('<style>.bg { background-image: url(\'/static/images/bg.jpg\'); } h1 { text-shadow: #000\n0px 0px 16px;\n}</style><div class="radial-box-dark"><h1>Classic With An Edge</h1><p>Finite Designer Jewelry is about creating unique high quality pieces of jewelry\nusing silver, gold and precious stones that represent valuable beauty at an\naffordable\nprice.</p><a href="/jewelry" class="button-gold">View Jewlery</a></div>');
+    out.w('<style>.bg { background-image: url(\'/static/images/bg.jpg\'); } .radial-box-dark {\nmargin-top: 64px; } h1 { text-shadow: #000 0px 0px 16px; } main { height: 100%;\n}</style><div class="radial-box-dark"><h1>Classic With An Edge</h1><p>Finite Designer Jewelry is about creating unique high quality pieces of jewelry\nusing silver, gold and precious stones that represent valuable beauty at an\naffordable\nprice.</p><a href="/jewelry" class="button-gold">View Jewlery</a></div>');
   };
 }
 (module.exports = require("marko").c(__filename)).c(create);
